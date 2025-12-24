f8bde7e6ef3bf699a6e88780226a8d8014dd44e7
Arjun Pangunuri <80937911+arjunshettyz@users.noreply.github.com>
2025-12-25 02:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (10.50%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,6 +1,6 @@
 class Solution {
     public int pivotInteger(int n) {
-        int ans = (n*n+n)/2;
+        int ans = n*(n + 1) / 2;
         int sq = (int)Math.sqrt(ans);
 
         if(sq*sq==ans) return sq;
