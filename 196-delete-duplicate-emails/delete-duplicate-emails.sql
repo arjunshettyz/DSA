a24b9fab314760a6fe33957e1d84b70ba829a8d0
Arjun Pangunuri <80937911+arjunshettyz@users.noreply.github.com>
2026-02-01 22:55:09 +0530

    Time: 579 ms (16.40%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,3 +1,6 @@
-# Write your MySQL query statement below
-delete p1 from person p1, person p2
-where p1.email = p2.email and p2.id < p1.id
+/* Write your T-SQL query statement below */
+delete p1
+from person p1
+join person p2
+on p1.email = p2.email
+and p1.id > p2.id;
